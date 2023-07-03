041ecd5342c5f994f1d0222e993a0cb1763ab8f2
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2023-07-03 22:06:49 +0200

    squeeze weights

--- utils/pytorch/DeepModel.py
+++ utils/pytorch/DeepModel.py
@@ -221,11 +221,12 @@ class Trainer(abc.ABC):
             print(len(w))
         with torch.no_grad():
             for i, (name, param) in enumerate(self.model.named_parameters()):
-                print(len(w[i]))
+
                 # print(np.shape(w[i]))
                 # p = w[i] if isinstance(w[i], np.ndarray) else np.array(w[i], dtype='float32')
                 # p = to_numpy(w[i], skip_obj_dtype=True)
                 p = to_numpy(w[i])
+                print(p.dtype)
                 # print(p.dtype)
                 # pdb.set_trace()
                 param.data = torch.from_numpy(p).to(device=self.device)
