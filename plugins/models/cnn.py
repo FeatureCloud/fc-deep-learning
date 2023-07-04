35f3e6e978ac8bd8d1edc91da02120b82fc76801
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2023-07-04 10:54:48 +0200

    log softmax removed

--- plugins/models/cnn.py
+++ plugins/models/cnn.py
@@ -18,4 +18,5 @@ class Model(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x)
+        return x
+        # return F.log_softmax(x)
