bc1f3b8cf2d3cebdd05f5bdb6d31aadf8b62831b
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2023-07-04 10:44:52 +0200

    squeeze weights

--- utils/pytorch/DeepModel.py
+++ utils/pytorch/DeepModel.py
@@ -217,7 +217,7 @@ class Trainer(abc.ABC):
         """
         if len(w) == 1:
             print("w1",len(w[0]))
-            w=np.squeeze(w)
+            w=np.squeeze(w).tolist()
         else:
             print(len(w))
         print(self.device)
